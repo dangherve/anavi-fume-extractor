aacd23154d4aa6f33db170cce6fd07518530d208
dangherve <dangherve@gmail.com>
2026-03-07 01:25:22 -0500

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git remote add upstream https://github.com/AnaviTechnology/anavi-fume-extractor.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/anavi-fume-extractor.git
+git push --force --set-upstream origin master
